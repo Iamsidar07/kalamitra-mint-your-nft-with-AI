db1907e1f2347980ec67e0e11d1f01514057b7b8
Manoj Kumar <ms8460149@gmail.com>
2024-07-12 14:48:08 +0530

    improve ui

--- frontend/components/PromptExampleItem.tsx
+++ frontend/components/PromptExampleItem.tsx
@@ -19,13 +19,13 @@ const PromptExampleItem = ({
     toast("🫣 Prompt copied.");
   };
   return (
-    <div className="sm:h-72 flex flex-col md:flex-row p-4 filter backdrop-blur-3xl bg-zinc-800/30 ring-2 ring-zinc-800/80 bg-opacity-10 rounded-lg gap-4">
+    <div className="lg:h-72 flex flex-col lg:flex-row p-4 filter backdrop-blur-3xl bg-zinc-800/30 ring-2 ring-zinc-800/80 bg-opacity-10 rounded-lg gap-4">
       <Image
         src={imageUrl}
         alt={name}
         width={200}
         height={200}
-        className="object-cover w-full sm:w-[250px]"
+        className="object-cover w-full lg:w-[250px]"
       />
       <div className="h-full relative space-y-2">
         <h3 className="text-base md:text-lg">{name}</h3>
@@ -34,7 +34,7 @@ const PromptExampleItem = ({
         </p>
         <Button
           onClick={copyPrompt}
-          className="sm:absolute bottom-2 left-2 flex items-center gap-2"
+          className="lg:absolute bottom-2 left-2 flex items-center gap-2"
         >
           Copy
           <MdOutlineContentCopy />
